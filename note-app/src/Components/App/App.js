dee0dd206761736cbd03da5f62fb46763be7d059
Emily9903 <emily_9903@hotmail.com>
2022-06-21 10:26:45 +0100

    component files and .js added

---
+++ note-app/src/Components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Geta Note</h1>
+      <h3>Select a date or topic and share your notes</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ note-app/src/Components/Page 1/Page1.js
@@ -0,0 +1,9 @@
+export default function Page1 () {
+return (
+<div>
+<h1>Geta Note</h1>;
+<h3>Select a date or topic and share your notes</h3>;
+</div>
+)};
+
+
