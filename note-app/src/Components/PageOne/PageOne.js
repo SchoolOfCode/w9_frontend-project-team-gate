6fe91a8537fc9913d258c8b2c19f0c5dac04a6e1
Emily9903 <emily_9903@hotmail.com>
2022-06-22 14:17:08 +0100

    small changes

--- note-app/src/Components/PageOne/PageOne.js
+++ note-app/src/Components/PageOne/PageOne.js
@@ -1,21 +1,15 @@
 import Title from "./Title/Title";
-css-module
 import SubTitle from "./SubTitle/SubTitle";
 import Style from "./PageOne.module.css"
-
 import Sidebar from "../Sidebar/Sidebar";
-main
 
 export default function PageOne() {
   
   return (
     <div className={Style.PageOne}>
       <Title />
-
       <SubTitle />
-
       <Sidebar/>
- main
     </div>
   );
 }
