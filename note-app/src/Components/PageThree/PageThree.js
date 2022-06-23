81033205d4587abe6ea3b205fdb8196fa1b29387
Emily9903 <emily_9903@hotmail.com>
2022-06-23 12:04:17 +0100

    search result link working

--- note-app/src/Components/PageThree/PageThree.js
+++ note-app/src/Components/PageThree/PageThree.js
@@ -9,7 +9,16 @@ import React from "react";
 export default function PageThree(props) {
   const location = useLocation();
   const data = location.state;
+  //let navigate = useNavigate();
   //console.log(data);
+
+  // async function newDate(e) {
+  //   e.preventDefault();
+  //   let searchedDate = await fetch(`http://localhost:3000/notes/date/${text}`);
+  //   let searchResults = await searchedDate.json();
+  //   //console.log(searchResults);
+  //   navigate('/PageThree/', {state: searchResults.payload})
+  // }
 
   return (
     <div className={Style.PageThree}>
@@ -17,7 +26,7 @@ export default function PageThree(props) {
       {data && data.map((note) => (
        <Note key={note.id} date={note.date} topics={note.topics} notes={note.notes}></Note>
       ))}
-      {data && data.map((note) => {})}
+      {/* {data && data.map((note) => {})} */}
       <AddNotesButton />
       <NotesInput />
     </div>
